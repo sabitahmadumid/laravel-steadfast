195f3c1ba5db285ef9411602f5fb801b8e631947
Sabit Ahmad <sabitahmadumid@gmail.com>
2025-04-04 12:55:24 +0600

    Add SteadfastLog model for logging requests and responses

---
+++ src/Models/SteadfastLog.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace SabitAhmad\SteadFast\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class SteadfastLog extends Model
+{
+    protected $table = 'steadfast_logs';
+
+    protected $guarded = [];
+    protected $casts = [
+        'request' => 'array',
+        'response' => 'array',
+    ];
+
+
+
+}
